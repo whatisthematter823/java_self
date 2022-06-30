80e59ce59a524ed02628bb437072e820faae0813
강동관 <dpfwpf2999@naver.com>
2022-06-30 23:51:03 +0900

    220630 practice

---
+++ practice/220628/src/javaself/javatutorials/condition/Condition3Demo.java
@@ -0,0 +1,18 @@
+package javaself.javatutorials.condition;
+
+public class Condition3Demo {
+
+	public static void main(String[] args) {
+//        if (true) {
+//            System.out.println(1);
+//        } else {
+//            System.out.println(2);
+//        }
+        if(false){
+            System.out.println(1);
+        } else {
+            System.out.println(2);
+        }
+	}
+
+}
---
+++ practice/220628/src/javaself/javatutorials/condition/ConditionDemo.java
@@ -0,0 +1,11 @@
+package javaself.javatutorials.condition;
+
+public class ConditionDemo {
+
+	public static void main(String[] args) {
+		if(true){ // if절
+		    System.out.println("result : true"); // then절
+		}
+	}
+
+}
